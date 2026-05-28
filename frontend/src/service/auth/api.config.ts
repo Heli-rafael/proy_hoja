6f56d2a992f59c721bd57fd06a22bd7c179dae76
HELI HIDETOSHI RAFAEL RAMON <2022110583@udh.edu.pe>
2026-05-28 14:50:55 -0500

    Configuracion settings

--- frontend/src/service/auth/api.config.ts
+++ frontend/src/service/auth/api.config.ts
@@ -1,7 +1,7 @@
 export class ApiConfig {
-  static readonly apiUrl = 'http://10.84.207.214:8000/';
+  static readonly apiUrl = 'https://proy-hoja.duckdns.org/';
   static readonly apiMedia = 'http://localhost:8000/media/';
   static readonly apikey = 'clave';
 }
 
-// https://backendhr.duckdns.org/api/
+// https://proy-hoja.duckdns.org/
