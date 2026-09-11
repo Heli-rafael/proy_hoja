50b18c5857b878f880a653d627a06d72316ce395
HELI HIDETOSHI RAFAEL RAMON <2022110583@udh.edu.pe>
2026-09-11 14:35:56 -0500

    Configuraciones

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,21 +59,21 @@ ALLOWED_HOSTS = env(
 SITE_URL = env("SITE_URL", default="http://localhost:8000")
 
 # SERVICIOS
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GOOGLE_CLIENT_ID = env("GOOGLE_CLIENT_ID", default=None)
+OPENAI_API_KEY = env("OPENAI_API_KEY", default=None)
+GEMINI_API_KEY = env("GEMINI_API_KEY", default=None)
 
 # OTP EMAIL
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST = env("EMAIL_HOST", default=None)
+EMAIL_PORT = int(env("EMAIL_PORT", default=587))
+EMAIL_USE_TLS = env("EMAIL_USE_TLS", default="True") == "True"
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default=None)
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default=None)
 
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default=None)
 
 # Application definition
 INSTALLED_APPS = [
